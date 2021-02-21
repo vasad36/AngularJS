458c7750243c571e9538e2357e4167069be60503
Alpesh Patanwadia <vasad36@gmail.com>
2021-02-22 02:25:15 +0530

    Add project files.

---
+++ Controllers/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('DemoApp', []);
+
+app.controller('DemoController', function ($scope) {
+    $scope.tutorialName = "Angular JS";
+    $scope.msg = "This is Test Variable!";
+
+});
+
+app.controller('DemoController', function ($scope) {
+    $scope.fname = "Alpesh";
+    $scope.lname = "Patanwadia";
+    var fname1 = $scope.fname;
+    var lname1 = $scope.lname;
+    $scope.fullname = function (fname1, lname1) {
+        return fname1 + lname1;
+    }
+  
+});
---
+++ Controllers/repete.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('DemoApp', []);
+
+app.controller('DemoController', function ($scope) {
+
+    $scope.Topicname = [
+        { name: "This is Test1" },
+        { name: "This is Test2" },
+        { name: "This is Test3" },
+            ]
+
+});

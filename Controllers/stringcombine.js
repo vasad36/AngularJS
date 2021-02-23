f1d29b4df2ecbd8ad19e1bc8b3010ea77341dca3
Alpesh Patanwadia <vasad36@gmail.com>
2021-02-24 00:37:44 +0530

    BIt- Lecture 4

---
+++ Controllers/stringcombine.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('TestApp', []);
+
+app.controller('cont_Add', function ($scope) {
+
+    
+    $scope.secondvar = function () {
+        $scope.returnval = $scope.val1 + $scope.val2;
+        return returnval;
+    }
+
+});
+
+
